6f7e82b1df02116cbf7622345a351c642ca1b484
Tomé Vilela <tome.vilela@gmail.com>
2016-04-07 16:36:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Frases Curtas
+
+Este repositório contém a estrutura básica do site http://frasescurtas.org, um micro site que desenvolvi.
+
+## Como funciona?
+
+O site [Frases Curtas](http://frasescurtas.org) implementa um template simples usando HTML, CSS e JavaScript puros para a visualização e compartilhamento de frases relevantes e interessantes no idioma português (pt-BR).
+
+Há integração com Facebook para que os usuários possam curtir e compartilhar as frases com seus amigos, e também é possível enviar um link customizado de cada frase via WhatsApp através de um botão que segue o schema de URL do aplicativo, conforme apresentado abaixo:
+
+```
+whatsapp://send?text=Hello%2C%20World!
+```
+
+Pensando que boa parte dos acessos ao [Frases Curtas](http://frasescurtas.org) será feito através de dispositivos mobile, as páginas do site foram feitas bastante enxutas, e o layout foi trabalhado de modo a ser responsivo para oferecer uma boa experiência de navegação. Veja um exemplo abaixo:
+
+![http://imgur.com/QTmKUHq](http://i.imgur.com/QTmKUHq.png?1)
+
+Esperamos que gostem das frases!
+
+### Contato
+
+Fique à vontade para entrar em contato através do e-mail tome.vilela@gmail.com
---
+++ js/all.js
@@ -0,0 +1,40 @@
+﻿
+var enableGoogleAnalytics = true;
+
+var __fbInit = window.fbAsyncInit;
+window.fbAsyncInit = function () {
+    __fbInit();
+
+    FB.Event.subscribe('edge.create', function () {
+        ga('send', 'event', 'Social', 'Like+1', 'Facebook');
+    });
+
+    FB.Event.subscribe('edge.remove', function () {
+        ga('send', 'event', 'Social', 'Like-1', 'Facebook');
+    });
+};
+
+function randomPhrase() {
+    var vector = [18, 17, 49, 25, 28, 4, 9, 8, 32, 25, 28, 9, 16, 8, 28, 23, 9, 16, 5, 26, 15, 6, 8, 18, 8, 17, 16, 17, 10, 6, 20, 18, 6, 8, 10, 9, 16, 15, 46, 15, 8, 25, 26, 8, 7, 14, 6, 15, 26, 8, 7, 5, 24, 7, 47, 20, 22, 17, 36, 4, 14, 6, 5, 16, 13, 34, 55, 36, 20, 15, 23, 24, 6, 19];
+    var r1 = Math.floor(Math.random() * vector.length);
+    var r2 = Math.floor(Math.random() * vector[r1]);
+    location.href = '../' + (r1 + 1) + '/' + (r2 + 1) + '.html';
+}
+
+function sendViaWhatsApp(e) {
+    ga('send', 'event', 'Social', 'Share', 'WhatsApp');
+    location.href = "whatsapp://send?text=" + location.href;
+    e.preventDefault();
+}
+
+function getMetaTag(property) {
+    var metas = document.getElementsByTagName('meta');
+
+    for (var i = 0; i < metas.length; i++) {
+        if (metas[i].getAttribute("property") == property) {
+            return metas[i].getAttribute("content");
+        }
+    }
+
+    return "";
+}
